67d8a7301dd83517905fad57c7e7058f5a7617b3
Ytenok2024 <nikolajbezruk45@gmail.com>
2024-08-11 18:10:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Обслуживание статических файлов из папки public
-app.use(express.static('public'));
+
 
 const saltRounds = 10;
 
